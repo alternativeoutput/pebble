dbce7198efd426e47aeb6ba04a1bd6bf2bba1333
Matteo Nastasi <nastasi@alternativeoutput.it>
2018-08-19 15:02:18 +0200

    extract App from components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDom from 'react-dom';
 import { Provider } from "react-redux";
 import store from "./store/index";
-import App from "./components/App";
+import App from "./App";
 import './index.css';
 ReactDom.render(
   <Provider store={store}>
@@ -10,13 +10,3 @@ ReactDom.render(
   </Provider>,
   document.getElementById("root")
 );
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
